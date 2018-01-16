734e6d4cca47aec24c32da401e511a6a1b1a252a
Ze <li111169@yahoo.com>
2018-01-16 11:28:41 -0500

    Add files via upload

--- Six_Phase_SVPWM_Vector.m
+++ Six_Phase_SVPWM_Vector.m
@@ -7,7 +7,14 @@ coord_value=planes*switch_vectors';%the plane sequence is [alpha beta z1 z2], sw
 vector_mag=abs(sqrt(coord_value([1,3],:).^2+coord_value([2,4],:).^2));
 aubvcw=switch_vectors;
 abcuvw=[switch_vectors(:,[1 3 5]) switch_vectors(:,[2 4 6])];
-vector_info=roundn([coord_value' abcuvw vector_mag'],-2);
+% vector_info=roundn([coord_value' abcuvw vector_mag'],-2);
+vector_info.coord_value=roundn(coord_value',-2);
+vector_info.abcuvw=roundn(abcuvw,-2);
+vector_info.vector_mag=roundn(vector_mag',-2);
+vector_info.flag=zeros(64,1);
+vector_info.flag(vector_info.vector_mag(:,1)==1)=1;
+index=find(vector_info.flag==1);
+vector_info.coord_value(index,[1 2]);
 
 axis equal
 alpha_beta=plot(coord_value(1,:)',coord_value(2,:)','*');
