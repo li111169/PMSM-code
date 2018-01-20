c7d8e205da645cccc79bdce8873c44bd7f5d8081
Ze <li111169@yahoo.com>
2018-01-19 21:37:06 -0500

    Add files via upload

--- Six_Phase_SVPWM_Vector.m
+++ Six_Phase_SVPWM_Vector.m
@@ -7,20 +7,26 @@ coord_value=planes*switch_vectors';%the plane sequence is [alpha beta z1 z2], sw
 vector_mag=abs(sqrt(coord_value([1,3],:).^2+coord_value([2,4],:).^2));
 aubvcw=switch_vectors;
 abcuvw=[switch_vectors(:,[1 3 5]) switch_vectors(:,[2 4 6])];
-% vector_info=roundn([coord_value' abcuvw vector_mag'],-2);
+
 vector_info.coord_value=roundn(coord_value',-2);
 vector_info.abcuvw=roundn(abcuvw,-2);
 vector_info.vector_mag=roundn(vector_mag',-2);
 vector_info.flag=zeros(64,1);
 vector_info.flag(vector_info.vector_mag(:,1)==1)=1;
+
 index=find(vector_info.flag==1);
-vector_info.coord_value(index,[1 2]);
+rep_element=vector_info.coord_value(index,[1 2]);
+[~,rep_index]=Check_Repetition(rep_element);
+
 
 axis equal
 alpha_beta=plot(coord_value(1,:)',coord_value(2,:)','*');
 xlim([-2.5 2.5]);
 ylim([-2.5 2.5]);
-text(coord_value(1,:)'+0.1,coord_value(2,:)',dec2base(bin2dec(num2str(abcuvw)),8));
+text(coord_value(1,vector_info.flag==0)'+0.1,coord_value(2,vector_info.flag==0)',dec2base(bin2dec(num2str(vector_info.abcuvw(vector_info.flag==0,:))),8));
+
+text(vector_info.coord_value(rep_index,1)+0.1,vector_info.coord_value(rep_index,2),dec2base(bin2dec(num2str(vector_info.abcuvw(vector_info.coord_value,:))),8));
+
 % subplot(1,2,2)
 % plot(coord_value(3,:)',coord_value(4,:)','*');
 
